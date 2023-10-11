4b06f6d4f6b5100363e404f9a8af47c0c9674a69
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2023-10-11 23:23:33 +0200

    Update chess board file (frontend)
    made changes on template!

---
+++ chessboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PHP Chess Board</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="keywords" content="PHP"/>
+        <meta name="description" content="PHP, Practice, GadCodes, ChessBoard"/>
+    </head>
+    <body>
+        <h1>Chess Board using Nested For Loop</h1>
+        <table style="board-table">
+        
+        </table>
+    </body>
+</html>
