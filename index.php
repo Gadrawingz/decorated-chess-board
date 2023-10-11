f19da21373313fd2c918703dc858722087fc8640
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2023-10-11 23:54:21 +0200

    Created index.php
    Act of creating index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PHP Chess Board</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="keywords" content="PHP"/>
+        <meta name="description" content="PHP, Practice, GadCodes, ChessBoard"/>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h1>Decorated Chess Board</h1>
+        <table class="board">
+        <?php
+        include('chessboard.php');
+        ?>
+        </table>
+    </body>
+</html>
